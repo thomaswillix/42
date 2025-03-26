0e1b3d95a2e9e8169d9bc9fc584f64b4c9d6eb45
Thomas <117021963+thomaswillix@users.noreply.github.com>
2025-03-26 20:54:53 +0100

    Update ft_split.c

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -11,31 +11,105 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
-char **ft_split(char const *s, char c);
-static int num_elements(const char *s, char c);
-
-char	**ft_split(char const *s, char c)
+static int      count_words(char const *str, char c)
 {
-	char**	str;
-	int	num;
+        int     count;
+        int     i;
 
-	num = num_elements(s, c);
-	str = malloc()
+        count = 1;
+        i = 0;
+        while (str[i])
+        {
+                if (str[i] == c)
+                        count++;
+                i++;
+        }
+        return (count);
 }
 
-static int num_elements(const char *str, char c)
+static char     **allocate_words(int count)
 {
-    int i;
-    int	num;
+        char    **result;
 
-    i = 0;
-    num = 1;
-    while (str[i + 1])
-    {
-        if (c == set[i] && c != set[i+1])
-		num++;
-	i++;
-    }
-    return (num);
+        result = (char **) malloc((count + 1) + sizeof(char *));
+        if (!result)
+                return (NULL);
+        return (result);
+}
+
+
+
+static char     *copy_segment(char const *str, int start, int len)
+{
+        char    *segment;
+        int             k;
+
+        k = 0;
+        segment = (char *) malloc(len + 1);
+        if (!segment)
+                return (NULL);
+        while (k < len)
+        {
+                segment[k] = str[start + k];
+                k++;
+        }
+        segment[k] = '\0';
+        return (segment);
+}
+
+static void     fill_words(char **result, char const *str, char c)
+{
+        int     i;
+        int     j;
+        int     start;
+        int     len;
+
+        i = 0;
+        j = 0;
+        start = 0;
+        len = 0;
+        while (str[i])
+        {
+                if (str[i] == c || str[i + 1] == '\0')
+                {
+                        len = i - start + (str[i + 1] == '\0');
+                        result[j] = copy_segment(str, start, len);
+                        if (!result[j])
+                                return ;
+                        j++;
+                        start = i + 1;
+                }
+                i++;
+        }
+}
+
+char    **ft_split(char const *s, char c)
+{
+        char    **result;
+        int             count;
+
+        count = count_words(s, c);
+        result = allocate_words(count);
+        if (!result)
+                return (NULL);
+        fill_words(result, s, c);
+        return (result);
+}
+
+#include <stdio.h>
+int main ()
+{
+        char **result;
+        char str[] = "Hola ,muy buenasss";
+        char sep = ' ';
+
+        result = ft_split(str, sep);
+        int i = 0;
+        while (result[i]){
+                printf("Parte %d: %s\n", i, result[i]);
+                i++;
+        }
+        return (0);
 }
